4eb6877064f61d4a136fdb1a29fafbcc72cca218
“dipto0079” <sudiptobiswas423@gmail.com>
2023-05-15 00:38:47 +0600

    Livewire Installation

---
+++ app/Http/Livewire/Footer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Footer extends Component
+{
+    public function render()
+    {
+        return <<<'blade'
+            <div>
+                <h1>test app</h1>
+            </div>
+        blade;
+    }
+}
---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,6 @@
+<div style="text-align: center">
+    <h1>test All</h1>
+    <button wire:click="increment">+</button>
+    <h1>{{$count}}</h1>
+    <button wire:click="decrement">-</button>
+</div>
